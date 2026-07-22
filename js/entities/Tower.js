9efc808ccd0994727789aa2fccd68af93b36ed2d
wzc51 <wzc51@users.noreply.github.com>
2026-07-22 19:42:33 +0800

    v0.1.7: 塔比例修正(0.07匹配平台)+路径y坐标校准道路中心

--- js/entities/Tower.js
+++ js/entities/Tower.js
@@ -19,7 +19,7 @@ class Tower extends Phaser.GameObjects.Container {
 
     // 塔本体：使用 PNG 素材，比例调大让底座匹配塔位圈（半径28px ≈ 直径56px）
     this.towerSprite = scene.add.sprite(0, 0, config.imageKey);
-    this.towerSprite.setScale(0.12);
+    this.towerSprite.setScale(0.07);
     this.add(this.towerSprite);
 
     // 攻击范围圈
@@ -47,7 +47,7 @@ class Tower extends Phaser.GameObjects.Container {
     this.range = Math.floor(this.range * 1.1);
     this.attackSpeed = Math.floor(this.attackSpeed * 0.85);
 
-    this.towerSprite.setScale(0.14);
+    this.towerSprite.setScale(0.09);
 
     this.rangeCircle.clear();
     this.rangeCircle.lineStyle(1.5, this.color, 0.2);
@@ -169,7 +169,7 @@ class Tower extends Phaser.GameObjects.Container {
       hpBarBg: this.scene.add.graphics()
     };
 
-    golem.sprite.setScale(0.20);
+    golem.sprite.setScale(0.12);
     golem.sprite.setDepth(4);
 
     // 血条
@@ -179,8 +179,8 @@ class Tower extends Phaser.GameObjects.Container {
     golem.sprite.setScale(0);
     this.scene.tweens.add({
       targets: golem.sprite,
-      scaleX: 0.20,
-      scaleY: 0.20,
+      scaleX: 0.12,
+      scaleY: 0.12,
       duration: 400,
       ease: 'Back.easeOut'
     });
